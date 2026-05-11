dd08a1fe565041d445458c8f36209094618fbc5a
thomaspelletier955-sudo <thomas.pelletier955@gmail.com>
2026-05-11 23:24:46 +0200

    Update enseignant.h

--- enseignant.h
+++ enseignant.h
@@ -7,3 +7,13 @@
  authentification création et sauvegarde de QCM
 */
 
+#include "utils.h"
+ 
+int  verifier_mot_de_passe(void);
+void menu_enseignant(void);
+void creer_qcm(void);
+int  sauvegarder_qcm(const QCM *qcm);
+void saisir_question(Question *q, int reponses_multiples);
+ 
+#endif
+ 
