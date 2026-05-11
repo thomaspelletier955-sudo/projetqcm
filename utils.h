0ad8667fabf7f3ae0b6dbb370e6c1732a97e45d8
thomaspelletier955-sudo <thomas.pelletier955@gmail.com>
2026-05-11 23:13:18 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -7,3 +7,19 @@
  utilitaires partagées dans tout le projet
  code couleur ANSI
 */
+/* Mot de passe enseignant */
+#define MOT_DE_PASSE "cytech2025"
+
+
+/* Codes couleurs ANSI */
+#define RESET    "\033[0m"
+#define GRAS     "\033[1m"
+#define ROUGE    "\033[31m"
+#define VERT     "\033[32m"
+#define JAUNE    "\033[33m"
+#define BLEU     "\033[34m"
+#define MAGENTA  "\033[35m"
+#define CYAN     "\033[36m"
+#define BLANC    "\033[37m"
+#define FOND_BL  "\033[44m"
+#define FOND_MG  "\033[45m"
