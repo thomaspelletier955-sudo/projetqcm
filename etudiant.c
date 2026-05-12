643ae0c9a631608c3eab2f5b725c949da983a5c8
thomaspelletier955-sudo <thomas.pelletier955@gmail.com>
2026-05-12 17:54:23 +0200

    Update etudiant.c

--- etudiant.c
+++ etudiant.c
@@ -4,9 +4,6 @@
  mode étudiant : listing des QCM
  chargement depuis fichier, qcm interactif et calcul automatique de la note sur 20 points.
 */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include <stdio.h>
 #include <stdlib.h>
